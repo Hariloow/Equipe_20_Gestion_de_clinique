0bc1fd1de3b2e617eef07a6f71273da0d64a872f
Hariloow <soleilhavoup.alban@gmail.com>
2024-09-09 19:58:00 -0400

    Premier Commit

---
+++ src/main/java/org/example/Clinic.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Clinic {
+    public Clinic(/* ... */) {
+    }
+
+    public void triagePatient(String name, int gravity, VisibleSymptom visibleSymptom) {
+    }
+}
