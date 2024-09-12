55824f6df2c7cc41833b9f7ff6daaf76e0314b7c
Hariloow <soleilhavoup.alban@gmail.com>
2024-09-12 15:08:35 -0400

    Etape 2 scenario 2

--- src/test/java/org/example/ClinicTest.java
+++ src/test/java/org/example/ClinicTest.java
@@ -68,4 +68,18 @@ public class ClinicTest {
 
         assertEquals(clinic.getPatientMedecin(), patientDoe);
     }
+
+    @Test
+    public void ajouterPatientMedecinGravityRadioFifoPasVide_avecBB7_trier(){
+        Clinic clinic = new Clinic(TriageType.GRAVITY, TriageType.FIFO);
+
+        Patient patientJohn = new Patient("John",4 , VisibleSymptom.BROKEN_BONE);
+        Patient patientDoe = new Patient("Doe", 7, VisibleSymptom.BROKEN_BONE);
+
+        clinic.triagePatient(patientJohn.getName(), patientJohn.getGravite(), patientJohn.getSymptom());
+        clinic.triagePatient(patientDoe.getName(), patientDoe.getGravite(), patientDoe.getSymptom());
+
+        assertEquals(clinic.getPatientRadio(), patientJohn);
+        assertEquals(clinic.getPatientRadio(), patientDoe);
+    }
 }
